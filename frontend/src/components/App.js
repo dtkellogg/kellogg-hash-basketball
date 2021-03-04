ce0e57df70a6debf15b18944ec32c20bfa243b22
Toshi <dtkellogg10@gmail.com>
2021-03-03 23:16:48 -0800

    Initial commit

---
+++ frontend/src/components/App.js
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import Players from "./Players";
+import Teams from "./Teams";
+import Navbar from "./Navbar";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/players">
+            <Players />
+          </Route>
+          <Route path="/teams">
+            <Teams />
+          </Route>
+          <Route path="*">
+            <h1 className="text-center">404</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
