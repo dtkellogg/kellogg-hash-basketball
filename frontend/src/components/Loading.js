ba6dde4ce27349ad417a209a85d2cfd8c8b59410
Toshi <dtkellogg10@gmail.com>
2021-11-11 15:10:57 -0800

    Not all files were added in the last commit. Adding them now.

---
+++ frontend/src/components/Loading.js
@@ -0,0 +1,39 @@
+import React, {useState, useEffect} from "react";
+
+// prop-types
+import PropTypes from "prop-types";
+
+
+export default function Loading({ text = "Loading", speed = 300 }) {
+  const [content, setContent] = useState(text);
+
+  if(document.title !== "Toshi Kellogg | Developer") {
+    document.title = "Toshi Kellogg | Developer";
+  }
+
+  useEffect(() => {
+    if (content.charAt(0) !== "L") {
+      document.title = "Toshi Kellogg | Developer";
+    } else {
+      document.title = content;
+    }
+  }, [content]);
+
+  useEffect(() => {
+    const id = window.setInterval(() => {
+      setContent((content) => {
+        return content === `${text}...` ? text : `${content}.`;
+      });
+    }, speed);
+
+    return () => window.clearInterval(id);
+  }, [text, speed]);
+  
+
+  return <div className="dots-loader"></div>;
+}
+
+Loading.propTypes = {
+  text: PropTypes.string,
+  speed: PropTypes.number,
+};
